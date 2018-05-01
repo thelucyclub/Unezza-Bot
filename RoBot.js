e5c6f37460da8b36fe8f1abedaa41689ac2c05fc
TheLucyClub Github Team <thelucyclub@gmail.com>
2018-05-01 13:21:06 -0400

    Update RoBot.js

--- RoBot.js
+++ RoBot.js
@@ -48,7 +48,7 @@ ready = 1;
 function SetRank(userId){
 	console.log("Setting rank of \""+userId+"\"");
 	//rbx.setRank(, userId, 2);
-	request.post({url:'https://www.roblox.com/groups/api/change-member-rank?groupId=3620561&newRoleSetId=2&targetUserId=' + userId}, function (err, resp, bod){
+	await request.post({url:'https://www.roblox.com/groups/api/change-member-rank?groupId=3620561&newRoleSetId=2&targetUserId=' + userId}, function (err, resp, bod){
 		console.log(err);
 		console.log(resp);
 		console.log(resp.code);
