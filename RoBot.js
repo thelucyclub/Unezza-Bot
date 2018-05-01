02af15415a1070fcdf952160274ac66989608e27
TheLucyClub Github Team <thelucyclub@gmail.com>
2018-04-30 21:07:22 -0400

    Update RoBot.js

--- RoBot.js
+++ RoBot.js
@@ -73,7 +73,7 @@ try {
 	   regexp = regexp.substr(1, regexp.length - 2);
        message.reply("Verifying as \""+regexp+"\"...");
 		console.log("Pre-verify");
-	   if (!regexp) message.reply("You need to be verified with RoVer to use this command.\nPlease run `!verify` in <#402320341654962176>."); message.channel.stopTyping(); return;
+	   if (!regexp) console.log("User not verified `"+regexp+"`"); message.reply("You need to be verified with RoVer to use this command.\nPlease run `!verify` in <#402320341654962176>."); message.channel.stopTyping(); return;
 	   console.log("requesting resource");
 		try{request(`https://api.roblox.com/users/get-by-username?username=${regexp}`, function (error, response, body){
 		   if(error) message.reply("UMX_RESPONSE_INVALID::" + response); return;
